29f81d4c0e4ead3a0e8fcc4422767ec9762754bf
krakissi <mike@krakissi.net>
2018-11-10 00:04:21 -0500

    debug - dump players from database

--- src/dbpersistence.c
+++ src/dbpersistence.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <mysql/mysql.h>
@@ -20,6 +21,16 @@ MYSQL *get_conn(){
 	return mysql;
 }
 
+void debug_print_character(FILE *stream, player *pl){
+	fprintf(stream, "%2ld: %s (%s) created=%ld pass=%s\r\n",
+		pl->id_player,
+		pl->name,
+		pl->nick,
+		pl->created,
+		pl->pass
+	);
+}
+
 void db_dump_users(FILE *stream){
 	MYSQL *mysql = get_conn();
 
@@ -36,17 +47,59 @@ void db_dump_users(FILE *stream){
 		if(!result)
 			return;
 
-		int fields = mysql_num_fields(result);
+		mysql_num_fields(result);
 		MYSQL_ROW row;
 
-		while((row = mysql_fetch_row(result))){
-			for(int i = 0; i < fields; i++)
-				fprintf(stream, "\"%2s\" ", row[i] ? row[i] : "NULL");
+		linkedlist players = malloc(sizeof(listlink)), players_it = players;
+		players->next = NULL;
+		players->el = NULL;
+		player *pl;
 
-			fprintf(stream, "\r\n");
+		while((row = mysql_fetch_row(result))){
+			if(!players_it->el)
+				players_it->el = malloc(sizeof(player));
+			pl = (player*) players_it->el;
+
+			pl->id_player = atol(row[0]);
+			strcpy(pl->name, row[1]);
+			strcpy(pl->nick, (row[2] ? row[2] : row[1]));
+
+			// FIXME convert row[3] timestamp to long val.
+			pl->created = 0;
+
+			strcpy(pl->pass, row[4]);
+
+			players_it = players_it->next = malloc(sizeof(listlink));
+			players_it->next = players_it->el = NULL;
 		}
 
 		mysql_free_result(result);
+
+		players_it = players;
+		while(players_it->el){
+			pl = (player*) players_it->el;
+			debug_print_character(stream, pl);
+
+			if(players_it->next)
+				players_it = players_it->next;
+			else break;
+		}
+		fprintf(stream, "\r\n");
+
+		players_it = players;
+		while(players_it->el){
+			free(players_it->el);
+
+			if(players_it->next){
+				players = players_it;
+				players_it = players_it->next;
+
+				free(players);
+			}
+		}
+		if(players_it)
+			free(players_it);
+
 	} else return; // Error
 
 	mysql_close(mysql);
--- src/dbpersistence.h
+++ src/dbpersistence.h
@@ -8,7 +8,20 @@
 #ifndef QS_DBPERSISTENCE_H
 #define QS_DBPERSISTENCE_H
 
+typedef struct player {
+	long id_player;
+	long created;
+
+	char name[9], nick[9], pass[65];
+} player;
+
+typedef struct listlink {
+	void *el;
+	struct listlink *next;
+} *linkedlist, listlink;
+
 // FIXME debug
 extern void db_dump_users(FILE *stream);
+extern void debug_print_character(FILE *stream, player *pl);
 
 #endif
